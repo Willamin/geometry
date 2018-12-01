eb9beb661106c4db33da00d9c24b03e40555e178
Will Lewis <will@wflewis.com>
2018-11-30 21:42:32 -0500

    Add more tests

--- spec/geometry_spec.cr
+++ spec/geometry_spec.cr
@@ -7,7 +7,14 @@ describe Geometry do
   test "line length" do
     a = Point.new(1, 2)
     b = Point.new(2, 2)
-    l = Line.new(a, b)
-    assert l.length == 1
+    c = Point.new(1, 1)
+    assert Line.new(a, b).length == 1
+    assert Line.new(b, a).length == 1
+
+    assert Line.new(a, c).length == 1
+    assert Line.new(c, a).length == 1
+
+    assert Line.new(b, c).length == Math.sqrt(2)
+    assert Line.new(c, b).length == Math.sqrt(2)
   end
 end
